ca6df2ebc8084fa61205b8605f7a4d6377834244
satokitty <tomohiko.s@gmail.com>
2022-12-12 01:49:14 +0900

    Implements graceful shutdown.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
@@ -16,11 +21,26 @@ func main() {
 	mux := http.NewServeMux()
 	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
 	mux.Handle(path, handler)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: h2c.NewHandler(mux, &http2.Server{}),
+	}
 
-	fmt.Println("Start server...")
+	go func() {
+		log.Printf("Start server at %s...\n", server.Addr)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalln("Server closed with error:", err)
+		}
+	}()
 
-	http.ListenAndServe(
-		":8080",
-		h2c.NewHandler(mux, &http2.Server{}),
-	)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	log.Printf("SIGNAL %d received, shutting down...\n", <-quit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("Graceful shutdown failed:", err)
+	}
+	log.Println("Server shutdown.")
 }
